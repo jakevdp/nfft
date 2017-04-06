4ee4faa6745ebf4387e3afcff208538f695cdb78
Jake VanderPlas <jakevdp@gmail.com>
2017-04-05 20:04:23 -0700

    TST: add tol tests

--- nfft/tests/test_core.py
+++ nfft/tests/test_core.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import pytest
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_less
 
 from ..core import indft, infft
 
@@ -14,13 +14,27 @@ def generate_data(N, amp=1, rseed=0):
 
 
 @pytest.mark.parametrize('N', [50, 100, 200])
+@pytest.mark.parametrize('Nf', [50, 100, 200])
 @pytest.mark.parametrize('sigma', [2, 3, 4, 5])
 @pytest.mark.parametrize('use_fft', [True, False])
 @pytest.mark.parametrize('use_sparse', [True, False])
-def test_infft_slow(N, sigma, use_fft, use_sparse):
+def test_infft_slow(N, Nf, sigma, use_fft, use_sparse):
     x, f = generate_data(N)
 
-    direct = indft(x, f, N)
-    slow = infft(x, f, N, sigma=sigma, use_fft=use_fft, use_sparse=use_sparse)
+    direct = indft(x, f, Nf)
+    approx = infft(x, f, Nf, sigma=sigma, use_fft=use_fft, use_sparse=use_sparse)
 
-    assert_allclose(direct, slow)
+    assert_allclose(direct, approx)
+
+
+@pytest.mark.parametrize('N', [50, 100, 200])
+@pytest.mark.parametrize('Nf', [50, 100, 200])
+@pytest.mark.parametrize('tol', [1E-4, 1E-8, 1E-12])
+@pytest.mark.parametrize('amp', [1, 10, 100])
+@pytest.mark.parametrize('sigma', [2, 3, 4, 5])
+def test_tol(N, Nf, tol, amp, sigma):
+    x, f = generate_data(N, amp=amp)
+    direct = indft(x, f, Nf)
+    approx = infft(x, f, Nf, sigma=sigma, tol=tol)
+    observed_diff = abs(direct - approx)
+    assert observed_diff.max() < tol * abs(f).sum()
