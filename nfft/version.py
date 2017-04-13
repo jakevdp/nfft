69f70d1974414615011104db3d07948862ae2f68
Jake VanderPlas <jakevdp@gmail.com>
2017-04-13 11:51:31 -0700

    MAINT: bump version to 0.2.dev

--- nfft/version.py
+++ nfft/version.py
@@ -6,10 +6,10 @@ from os.path import join as pjoin
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
-_version_minor = 1
+_version_minor = 2
 _version_micro = ''  # use '' for first of series, number for 1 and above
-# _version_extra = 'dev'
-_version_extra = ''  # Uncomment this for full releases
+_version_extra = 'dev'
+# _version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor]
@@ -33,17 +33,23 @@ description = "nfft: pure-python nonuniform fast Fourier transform"
 # Long description will go up on the pypi page
 long_description = """
 
-nfft
-====
+nfft package
+============
 nfft is a pure-python implementation of the nonuniform fast Fourier transform.
-Its performance is comparable to that of [pynfft](https://pypi.python.org/pypi/pyNFFT),
-but it contains no compiled code and requires no links to external C libraries:
-it depends only on [numpy](http://numpy.org) and [scipy](http://scipy.org).
+Its performance is comparable to that of pynfft_, but it contains no compiled
+code and requires no links to external C libraries, beyond standard
+dependencies on numpy_ and scipy_.
 
 For more information and links to usage examples, please see the
 repository README_.
 
 .. _README: https://github.com/jakevdp/nfft/blob/master/README.md
+
+.. _pynfft: https://pypi.python.org/pypi/pyNFFT
+
+.. _numpy: https://www.numpy.org
+
+.. _scipy: https://www.scipy.org
 
 License
 =======
